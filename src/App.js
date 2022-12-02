e62d111294e7e51b3a3e6c857284278fc9ece73a
Johnny_P <shinezl39@gmail.com>
2022-12-02 20:18:01 +0900

    canvastest

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useRef, useEffect } from "react";
 
 function App() {
+  const canvasRef = useRef(null);
+  const contextRef = useRef(null);
+
+  const [ctx, setCtx] = useState([]);
+  const [isDrawing, setIsDrawing] = useState(false);
+
+  useEffect(() => {
+
+    if(!canvasRef) return;
+
+    const ctx = canvasRef.current.getContext("2d");
+    const image = new Image();
+    image.src = "https://storage.needpix.com/rsynced_images/sea-1547609_1280.jpg";
+
+    image.onload = function() {
+      ctx.drawImage(image, 0, 0, 500, 500);
+    }
+  
+    /* const canvas = canvasRef.current;
+    canvas.width = window.innerWidth * 0.5;
+    canvas.height = window.innerWidth * 0.5;
+
+    const context = canvas.getContext("2d");
+    context.strokeStyle = "black";
+    context.lineWidth = 2.5;
+    contextRef.current = context;
+
+    setCtx(context); */
+  }, [canvasRef]);
+
+/*   const startDrawing = () => {
+    setIsDrawing(true);
+  }
+
+  const finishDrawing = () => {
+    setIsDrawing(false);
+  }
+
+  const drawing = ({ nativeEvent }) => {
+    const { offsetX, offsetY } = nativeEvent;
+
+    if(ctx) {
+      if(!isDrawing) {
+        ctx.beginPath();
+        ctx.moveTo(offsetX, offsetY);
+      }
+      else {
+        ctx.lineTo(offsetX, offsetY);
+        ctx.stroke();
+      }
+    }
+  } */
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='testbox'>
+        <canvas
+          className='test-canvas'
+          ref={canvasRef}
+          /* onMouseDown={startDrawing}
+          onMouseUp={finishDrawing}
+          onMouseMove={drawing}
+          onMouseLeave={finishDrawing} */>
+        </canvas>
+      </div>
     </div>
   );
 }
